edb8ee10e0d2f94e266ecd95bf4afdb27031ce37
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-03 22:30:33 +0300

    Introduction to sets and comprehanding their basic properities

---
+++ introduction_to_sets.py
@@ -0,0 +1,14 @@
+schools={"Berkeley","İTÜ","MIT","TU Delft","Stanford","Oxford"}
+
+print(schools)
+
+print(len(schools))
+
+setNumber={1,2,3,4,5,6,7,8,9}
+setBoolean={True,False,False,True}
+
+print(setNumber)
+print(setBoolean)
+
+setNumber=set((1,2,3,4,5,6,7,8,9))
+print(type(setNumber))
