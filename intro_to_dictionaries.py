0e15312e3413f802f1bc345f5c011f0a50b03ffb
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-05 23:23:52 +0300

    Intro to dictionaries

---
+++ intro_to_dictionaries.py
@@ -0,0 +1,18 @@
+car={
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "electric": False,
+  "colors": ["red", "white", "blue"]
+}
+
+print(car["brand"], car["model"], car["year"])
+print(car)
+print(type(car))
+
+person=dict(name="John", age=30, city="New York")
+
+print(f"name: {person['name']}, age: {person['age']}, city: {person['city']}")
+
+my_keys=car.keys()
+print(my_keys)
