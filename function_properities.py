95e9155fdcd09898c4da69f1e3d448b61a4f3599
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-08 01:25:09 +0300

    Another functions lesson covering nested functions, summoning function in a function and examples.

--- function_properities.py
+++ function_properities.py
@@ -21,3 +21,53 @@ def seperator(a,b,/,*,c,d):
    print(a+b+c+d)
 
 seperator(7,8,c=14,d=19)
+
+
+def control(number):
+   if (number>0):
+      result="Positive"
+   elif (number==0):
+      result="Neutral"
+   else:
+      result="Negative"
+   return result
+
+print(control(5))
+print(control(-16))
+print(control(0))
+
+
+def square(number):
+   result=number**2
+   return result
+
+def sum_and_square(x,y):
+   calculation=x+y
+   return square(calculation)
+
+result=sum_and_square(3,4)
+print(result)
+
+
+def outer_function(name):
+   def greeting():
+      return f"Hello, {name}!"
+   return greeting()
+
+print(outer_function("Barbaros"))
+
+
+def sum(x,y):
+   return x+y
+
+def multiply(x,y):
+   return x*y
+
+def calculation(x,y):
+   sum_result=sum(x,y)
+   multiply_result=multiply(x,y)
+   return sum_result,multiply_result
+
+
+result=calculation(12,5)
+print(result)
