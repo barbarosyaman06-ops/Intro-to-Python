f780e6fc1589a18cb3eb2c2820c1f5350d1a108a
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-16 01:37:39 +0300

    math module and the IEEE 754 remainder functions purpose

---
+++ math_module.py
@@ -0,0 +1,38 @@
+import math
+
+# def outer_function():
+#   x=7
+#   def inner_function():
+#     nonlocal x
+#     x+=12
+#     print("x in the inner function: ",x)
+#   inner_function()
+#   print("x inside the outer function",x)
+# outer_function()
+
+min_value=min(7,34,19,23,45)
+max_value=max(19,80,67,23,94)
+
+print(min_value)
+print(max_value)
+
+
+absoulte_value=abs(-45)
+print(absoulte_value)
+
+power_value=pow(2,7)
+
+print(power_value)
+
+
+print(math.e)
+print(math.inf)
+print(math.tau)
+print(math.pi)
+print(-math.pi)
+print(math.nan) #specifes the value as a non number
+
+
+radian_value=math.radians(90)
+
+print(math.sin(radian_value))
---
+++ math_module2.py
@@ -0,0 +1,17 @@
+import math
+
+print(math.ceil(1.3))
+
+print(math.floor(5.6))
+
+print(math.sqrt(9))
+
+print(math.isqrt(8)) #2
+
+
+print(math.trunc(8.7654534))
+
+
+print(math.remainder(8,2))
+
+print(math.remainder(19,4))
