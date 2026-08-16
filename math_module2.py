e23810804e5813d46bdc064771e8137271a72a98
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-17 01:30:59 +0300

    more math module methods and recalling probablity operation.

--- math_module2.py
+++ math_module2.py
@@ -15,3 +15,25 @@ print(math.trunc(8.7654534))
 print(math.remainder(8,2))
 
 print(math.remainder(19,4))
+
+
+
+#combination
+print(math.comb(7,4))
+
+print(math.copysign(8,-15))
+
+
+#distance
+p=[5]
+q=[3]
+print(math.dist(p,q))
+
+
+a=[1,2,3]
+b=[4,6,7]
+
+print(math.dist(a,b))
+
+
+print(math.erf(13))
