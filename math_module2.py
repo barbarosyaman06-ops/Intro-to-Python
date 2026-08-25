9657632755e829d723cac2b28d096bf89e5ee9aa
barbarosyaman16 <barbarosyaman06@gmail.com>
2026-08-25 15:16:01 +0300

    A regular session for math module. Learned about everyday/calculus level math methods to help me troughout this course.

--- math_module2.py
+++ math_module2.py
@@ -37,3 +37,43 @@ print(math.dist(a,b))
 
 
 print(math.erf(13))
+
+
+print(math.exp(34))
+
+print(math.exp(2))
+
+
+print(math.fabs(-78.9))
+
+print(math.factorial(4))
+
+print(math.fmod(20, 6))
+
+print(math.frexp(8))
+
+# a quite handy method
+
+numbers=[1,2,3,4,7,8]
+
+print(math.fsum(numbers))
+
+print(math.gamma(6))
+
+print(math.prod(numbers))
+
+print(math.gcd(4,6))
+
+print(math.hypot(5,12))
+
+print(math.isclose(8.005,8.450, abs_tol=0.4))
+print(math.isclose(8.005,8.450, abs_tol=0.5))
+
+
+print(math.isfinite(math.inf))
+
+print(math.isinf(math.inf))
+
+print(math.isnan(math.nan))
+
+print(math.log(2,4))
